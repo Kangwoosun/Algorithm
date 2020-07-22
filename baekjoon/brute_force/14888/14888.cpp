e9eeb5826a77155e9eb0c8d018a95fc2f527e390
Kangwoosun <kws981924@gmail.com>
2020-07-22 09:58:02 +0000

    PS add brute_force 14888

---
+++ baekjoon/brute_force/14888/14888.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main(){
+    
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int N, base, min, index = 0 ,input;
+    int oper[4], A[12];
+    long long result_max = INT_MIN, result_min = INT_MAX, result;
+    
+    
+    bool check;
+    vector<int> v, t;
+    
+    cin >> N;
+    
+    for(int i = 0; i<N; ++i){
+        cin >> input;
+        A[i] = input;
+    }
+    
+    for(int i =0; i<4; ++i){
+        cin >> input;
+        oper[i] = input;
+    }
+    /*
+     * [0] = +
+     * [1] = -
+     * [2] = *
+     * [3] = /
+     */
+     
+    for(int i = 0; i<4; ++i)
+       for(int j =0; j<oper[i]; ++j)
+           v.push_back(i);
+    
+    while(1){
+        
+        result = A[0];
+    
+        check = false;
+        min = INT_MAX;
+        
+        for(int i =0; i<N-1; i++){
+            
+            
+            switch(v[i]){
+            
+            case 0:
+                result += A[i+1];
+                break;
+            case 1:
+                result -= A[i+1];
+                break;
+            case 2: 
+                result *= A[i+1];
+                break;
+            case 3:
+                result /= A[i+1];
+                break;
+            default:
+                break;
+            }
+            
+        }
+        
+        if(result < result_min)
+            result_min = result;
+        
+        if(result > result_max)
+            result_max = result;
+        
+        
+        
+        for(int i = v.size()-1; i>0; --i){
+            
+            if(v[i-1] < v[i]){
+                
+                base = v[i-1];
+                t.assign(v.begin()+i-1, v.end());
+                v.erase(v.begin()+i-1, v.end());
+                
+                for(int j = 1; j<t.size(); ++j){
+                    
+                    if(base < t[j] && t[j] < min){
+                        min = t[j];
+                        index = j;
+                    }
+                    
+                }
+                
+                int tmp = t[index];
+                t[index] = t[0];
+                t[0] = tmp;
+                
+                sort(t.begin()+1, t.end());
+                
+                v.insert(v.end(), t.begin(), t.end());
+                
+                check = true;
+                
+                break;
+                
+            }
+            
+        }
+        
+        
+        if (check)
+            continue;
+        else
+            break;
+        
+        
+        
+    }
+    
+    cout << result_max << endl;
+    cout << result_min;
+    
+    return 0;
+}
