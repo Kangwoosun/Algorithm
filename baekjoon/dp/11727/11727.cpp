32a27d0015d9a7151da09af097fbf109a41dc238
Kangwoosun <kws981924@gmail.com>
2020-08-01 12:08:37 +0000

    PS add dp 11727

---
+++ baekjoon/dp/11727/11727.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int d[1001];
+int dp(int n){
+    d[1] = 1;
+    d[2] = 3;
+    
+    for(int i = 3; i<=n; ++i){
+        d[i] = (d[i-1] + d[i-2]*2) % 10007;
+    }
+    
+    return d[n];
+    
+}
+
+int main(){
+    int N;
+    cin >> N;
+    cout << dp(N);
+    
+}
