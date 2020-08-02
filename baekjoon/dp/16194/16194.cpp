666297112042e95ea403a7506ec89e2a630951e5
Kangwoosun <kws981924@gmail.com>
2020-08-02 10:48:51 +0000

    PS add dp 16194

---
+++ baekjoon/dp/16194/16194.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+int card[10001];
+int value[10001];
+
+int min(int a, int b){
+    return (a<b ? a : b);
+}
+int dp(int n){
+    
+    value[1] = card[1];
+    for(int i =2; i<=n; ++i){
+        value[i] = card[i];
+        for(int j =i-1; j>=(i+1)/2; --j){
+            value[i] = min(value[i], value[j] + value[i-j]);
+        }
+    }
+    
+    return value[n];
+}
+
+
+int main(){
+    int N, input;
+    
+    cin >> N;
+    
+    for(int i=1; i<=N; ++i){
+        cin >> input;
+        card[i] = input;
+    }
+    
+    cout << dp(N);
+}
