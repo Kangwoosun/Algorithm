f1f9e9798d6ce601b1d2509d31f4c46d4d24e712
Kangwoosun <kws981924@gmail.com>
2020-08-05 14:36:24 +0000

    PS add dp 11053

---
+++ baekjoon/dp/11053/11053.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int arr[1001];
+int d[1001];
+
+int max(int a, int b){
+    return (a>b?a:b);
+}
+
+int dp(int n){
+    int result = 0;
+    d[1] = 1;
+    
+    for(int i=1; i<=n;++i){
+        int min = 0;
+        
+        for(int j = 0; j<i; ++j){
+            
+            if(arr[i] > arr[j])
+                if(min < d[j])
+                    min = d[j];
+        }
+        d[i] = min+1;
+        result = max(result,d[i]);
+    }
+    
+    return result;
+    
+}
+
+
+int main(){
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    int N, input;
+    
+    cin >> N;
+    
+    for(int i =1; i<=N; ++i){
+        cin >> input;
+        arr[i] = input;
+    }
+    
+    cout << dp(N);
+    return 0;
+}
