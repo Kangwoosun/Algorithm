8ca002c24117cfeee7554bb719032756a95df12f
Kangwoosun <kws981924@gmail.com>
2020-08-06 08:27:11 +0000

    PS add dp 11055

---
+++ baekjoon/dp/11055/11055.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+
+
+int d[1001];
+int arr[1001];
+
+int max(int a, int b){
+    return (a>b?a:b);
+}
+
+int dp(int n){
+    
+    int result = 0;
+    d[1] = arr[1];
+    
+    for(int i = 1; i<=n; ++i){
+        int min =0;
+        
+        for(int j =0; j<i; ++j){
+            
+            if(arr[i] > arr[j])
+                if(min < d[j])
+                    min = d[j];
+            
+            
+        }
+        d[i] = min + arr[i];
+        result = max(result, d[i]);
+        
+    }
+    
+    return result;
+    
+}
+
+int main(){
+    
+    int N, input;
+    cin >> N;
+    
+    for(int i =1;i <= N; ++i){
+        cin >> input;
+        arr[i] = input;
+    }
+    
+    cout << dp(N);
+    
+    return 0;
+    
+}
