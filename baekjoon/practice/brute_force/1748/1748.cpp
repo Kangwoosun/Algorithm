1b2d898f4d381a9170d4545e5dca9f6ecf532aff
Kangwoosun <kws981924@gmail.com>
2020-08-12 10:50:46 +0000

    PS add brute_force 1748

---
+++ baekjoon/practice/brute_force/1748/1748.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int N,count = 0;
+    int i,idx = 0;
+    
+     cin >> N;
+    
+    for(i =10; i<N; i*=10){
+        idx++;
+        
+        cout << count << ' ' << idx << endl;
+        
+        count += idx *i/10*9;
+    }
+    idx++;
+    
+    i /= 10;
+    count += (N-i+1)*idx;
+    
+    cout << count;
+    
+    return 0;
+}
