c78543bbeb8db9517454938bcf87ffc1c33f65c9
Kangwoosun <kws981924@gmail.com>
2020-08-23 14:50:05 +0000

    SCPC diet solve

---
+++ baekjoon/practice/brute_force/2529/2529_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+char comp[10];
+bool check[10];
+int k;
+
+void max(){
+    
+    for(int i =0; i<k; ++i){
+        
+        if(comp[i] == '>'){
+            
+        }
+        
+        
+    }
+    
+}
+void min(){
+    
+}
+
+int main(){
+    
+    cin >> k;
+    
+    for(int i =0; i<k; ++i){
+        
+        cin >> comp[i];
+    }
+    
+    
+    
+    
+}
---
+++ baekjoon/practice/brute_force/6064/6064.cpp
@@ -0,0 +1,8 @@
+
+
+
+
+
+//https://joonas.tistory.com/25
+//https://www.crocus.co.kr/1232
+//https://core-research-team.github.io/?fbclid=IwAR1YCD3yHKvkAkJncpW-JRPS52MlbtMWFeFJWlXqMzCi5LMZkbZvyC69m2k
