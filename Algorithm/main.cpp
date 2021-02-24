b7a111036d718bf7b7d6f1af86743b1c8f321625
Kangwoosun <kws981924@gmail.com>
2021-02-24 23:38:54 +0900

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 강우선 on 2021/02/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
